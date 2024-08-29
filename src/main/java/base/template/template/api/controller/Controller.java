d3c344dbb0187ffe6a78c2ae2bb78a5768226ea4
Adam Maas <adamjm1220@gmail.com>
2024-08-29 11:37:15 -0400

    Barebones

--- src/main/java/base/template/template/api/controller/Controller.java
+++ src/main/java/base/template/template/api/controller/Controller.java
@@ -7,7 +7,6 @@ import base.template.template.api.model.Model;
 import base.template.template.service.TemplateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import base.template.template.service.S3Service;
 
 
 //Entire purpose of the controller is to expose the api endpoints and trigger the api
@@ -18,14 +17,10 @@ import base.template.template.service.S3Service;
 public class Controller {
     //Initialize new instance of the service for the controller
     private final TemplateService templateService;
-    private final S3Service s3Service;
 
     @GetMapping("/<Name_of_Endpoint_to_call_to_trigger_api>")
     public Model getModel(){
         Model model = templateService.getModel();
-        if (model != null) {
-            s3Service.uploadFile("<Name_Of_Your_S3_Bucket>", "<Name_Of_Your_File>.<File_Extension>", "<Name_Of_Your_File>.<File_Extension>");
-        }
         return model;
     }
 }
