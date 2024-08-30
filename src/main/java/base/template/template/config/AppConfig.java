e5104c142ac86b9fc08942327ad9156ca3a46cb3
Adam Maas <adamjm1220@gmail.com>
2024-08-30 09:47:38 -0400

    API_KEY_1_Docker

--- src/main/java/base/template/template/config/AppConfig.java
+++ src/main/java/base/template/template/config/AppConfig.java
@@ -1,6 +1,7 @@
 package base.template.template.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
@@ -11,6 +12,10 @@ import base.template.template.api.interceptor.ApiKeyInterceptor;
 //The entire purpose of the configuration file is to simply configure ther RestTemplate as a bean in the application s it can be autowired
 @Configuration
 public class AppConfig {
+
+    @Value("${api.key}")
+    private String apiKey;
+
     @Bean
     public RestTemplate restTemplate(ToExternalLoggingInterceptor toExternalLoggingInterceptor,IncommingLoggingInterceptor incommingLoggingInterceptor,ApiKeyInterceptor apiKeyInterceptor){
         RestTemplate restTemplate = new RestTemplate();
@@ -20,7 +25,7 @@ public class AppConfig {
 
     @Bean
     public ApiKeyInterceptor apiKeyInterceptor(){
-        return new ApiKeyInterceptor("<Add API Key Here>");
+        return new ApiKeyInterceptor(apiKey);
     }
 }
 
